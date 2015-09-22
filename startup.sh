3c8790dd76ba82f9058d3318342c277dcd9249e3
idealenergy <ideal.energy.advice@gmail.com>
2015-09-22 19:54:39 +0100

    Check for the update flag and git pull if it's there

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 echo "" > /etc/modprobe.d/raspi-blacklist.conf
+nohup bash util/deps.sh &> util/deps.log &
 sleep 5
 sudo modprobe i2c-dev
 sudo modprobe i2c-bcm2708
@@ -10,7 +11,14 @@ ls /dev/i2c*
 cat /etc/modules
 lsmod
 sudo i2cdetect -y 1
+cd /home/pi/pi
+upgradeflag=/var/run/ideal/upgrade
 #if [ $? -ne 0 ]; then
 #    shutdown -r now
 #fi
-node server.js
+if [ -f $upgradeflag ]; then
+    # grab software
+    git pull
+    rm $upgradeflag
+fi
+/usr/local/bin/node server.js
