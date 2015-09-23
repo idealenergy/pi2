d9a2a67539db16b91cf3ab0aa52ab2fdcd3d549e
idealenergy <ideal.energy.advice@gmail.com>
2015-09-23 17:49:33 +0100

    better upgrade flag location; make deps work again

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 echo "" > /etc/modprobe.d/raspi-blacklist.conf
-nohup bash util/deps.sh &> util/deps.log &
+BASEDIR=$(dirname $0)
+nohup bash $BASEDIR/util/deps.sh &> $BASEDIR/util/deps.log &
 sleep 5
 sudo modprobe i2c-dev
 sudo modprobe i2c-bcm2708
@@ -12,13 +13,15 @@ cat /etc/modules
 lsmod
 sudo i2cdetect -y 1
 cd /home/pi/pi
-upgradeflag=/var/run/ideal/upgrade
+upgradeflag=/home/pi/ideal.upgrade
 #if [ $? -ne 0 ]; then
 #    shutdown -r now
 #fi
 if [ -f $upgradeflag ]; then
     # grab software
+    echo "Requesting software from git repository..."
     git pull
-    rm $upgradeflag
+    sleep 5
+    sudo rm $upgradeflag
 fi
 /usr/local/bin/node server.js
