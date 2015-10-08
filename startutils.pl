c6c07600495dde85be8ef82e04990509c258b20f
idealenergy <ideal.energy.advice@gmail.com>
2015-10-08 14:07:12 +0100

    check main ideal service is running as well as appropriate USB services

--- startutils.pl
+++ startutils.pl
@@ -1,14 +1,21 @@
 #!/usr/bin/perl
 
+# Start services that should always be running - this will almost
+# always just report that the service(s) is(are) already running.
+my @services = ("ideal");
+for my $service (@services) {
+  `sudo service $service start`;
+}
+
 # Start processes to gather "other forms of data" - for example
 # CurrentCost data; HeatMeter data
-
 my @knownDataProviders = ("CurrentCost", "HeatMeter");
 my %processes = ("CurrentCost" => "./currentcost2ideal.pl", "HeatMeter" => "./heatmeter_receive.py");
 my %dmesgGrep = ("CurrentCost" => "pl2303", "HeatMeter" => "FTDI");
 
 for my $datasource (@knownDataProviders) {
-  # this likely won't work with e.g. memory sticks attached to Pi - check if it wrks after reboot..
+  # this likely won't work with e.g. memory sticks attached to Pi;
+  # also need to check if it works after reboot..
   my $resc = "dmesg | grep usb | grep attach | grep $dmesgGrep{$datasource} | tail -1";
   my $res = `$resc`; $res =~ s/\s*\n$//; $res =~s/^.*\s+//;
   my $commname = $processes{$datasource};
